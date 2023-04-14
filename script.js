5d18342e31e950592c380042d4baafe6a46a7781
heady8354 <williamdaboss3.5@outlook.com>
2023-04-13 20:14:08 -1000

    forest fight + choices mattering
    i should have done more today, but I have finally gotten ahold of sessionStorage and how it works, so a lot of choices matter now. I also have continued development on the new forest fight screen which should be done relatively easily, hopefully being done by the end of the week.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get the value of the variable from localStorage
-var myVariable = localStorage.getItem('myVariable') || false;
-var left = localStorage.getItem('left') || false;
+var myVariable = sessionStorage.getItem('myVariable') || false;
+var left = sessionStorage.getItem('left') || false;
 
 // Check the value of the variable and update the element on the other HTML page if necessary
 document.addEventListener('DOMContentLoaded', function() {
