1116d74eaa7889735aa0baf21b23b3bb428ae9af
heady8354 <williamdaboss3.5@outlook.com>
2023-04-22 18:53:03 -1000

    DONE!
    well, the time has come. i finished the game. It turned out to be significantly shorter than I wanted it to be, but this update I added double the amount of endings I wanted, so if you play through the whole game, it should be pretty long. Anyways, heres the list of all the changes:
    
    -Fixed goblins morale damage not being affected by their own morale
    -Buffed "Defend" in ForestFight
    -Added Coward Ending
    -Added Hero Ending
    -Added Villain Ending
    -Added Sacrifice Ending
    -Fixed sessionStorage issues regarding the ending
    
    and thats about all of it. For the rest of the time until the project is due I will be playtesting and getting reviews on it to further improve it. thank you for reading

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 // Get the value of the variable from localStorage
-var myVariable = sessionStorage.getItem('myVariable') || false;
-var left = sessionStorage.getItem('left') || false;
-var DJdead = sessionStorage.getItem('DJdead') || false
-var victory = sessionStorage.getItem('victory') || false
-var ignore = sessionStorage.getItem('ignore') || true
+var left = sessionStorage.getItem('left');
+if (left === null) {
+  left = false;
+}
+
+var DJdead = sessionStorage.getItem('DJdead');
+if (DJdead === null) {
+  DJdead = false;
+}
+
+var victory = sessionStorage.getItem('victory');
+if (victory === null) {
+  victory = false;
+}
+
+var ignore = sessionStorage.getItem('ignore');
+if (ignore === null) {
+  ignore = true;
+}
+var myVariable = sessionStorage.getItem('myVariable');
+if (myVariable === null) {
+  myVariable = false;
+}
 
 // Check the value of the variable and update the element on the other HTML page if necessary
 document.addEventListener('DOMContentLoaded', function() {
